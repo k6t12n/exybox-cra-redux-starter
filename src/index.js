6cdbe2121a337d7c4c053824523341bf4ecedf60
Khamphet AA-Master <khamphet046@gmail.com>
2021-07-27 23:43:43 +0700

    initial setup with redux and react-router-dom with auth setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import {
+    compose
+} from 'redux'
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+
+import Routes from 'routes'
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk, logger),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <React.StrictMode>
+            <Routes />
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
